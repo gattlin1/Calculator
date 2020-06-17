36bb4e71115360c80517c6427b36f7c099c658f9
gattlin1 <gattlin1@live.missouristate.edu>
2020-06-16 19:42:15 -0500

    Made rough outline for calculator (still need to style). Started working on keyboard input.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
+import { Element } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'calculator';
+  display: number;
+  numbers: Array<number>;
+
+  constructor() {
+    this.display = 0;
+    this.numbers = [...Array(10).keys()];
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  onKeyPress(event: KeyboardEvent) {
+    if (event.code.includes('Digit')) {
+      const aNumber = document.querySelector(`#${event.key}`);
+      console.log('Key Press');
+      console.log(event);
+      this.display++;
+    }
+  }
 }
